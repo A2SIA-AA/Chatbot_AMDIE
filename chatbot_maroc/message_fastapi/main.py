139f0c310e35f16e0bce1f10363d595acf5f82ba
A2SIA-AA <assia.ait_taleb@insa-rouen.fr>
2025-08-06 09:57:31 +0100

    changement http

--- chatbot_maroc/message_fastapi/main.py
+++ chatbot_maroc/message_fastapi/main.py
@@ -45,7 +45,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://10.35.100.119:3000",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
